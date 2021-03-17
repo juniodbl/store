268e18e4a1c058b834f94b1cd6b55269daf8aea5
juniodbl <juniodbl@hotmail.com>
2021-03-17 09:32:43 -0300

    init

---
+++ src/Api.js
@@ -0,0 +1,55 @@
+import Img from './100-180.svg';
+
+export const Api = { 
+    getItens: ()=>{
+        return [{
+            id: 1,
+            image: Img,
+            title: 'Copo',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 2,
+            image: Img,
+            title: 'Caneca',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 3,
+            image: Img,
+            title: 'Cafeteira',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 4,
+            image: Img,
+            title: 'Prato',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 5,
+            image: Img,
+            title: 'Copo',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 6,
+            image: Img,
+            title: 'Caneca',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 7,
+            image: Img,
+            title: 'Cafeteira',
+            fromValue: 15.00,
+            toValue: 10.00
+        },{
+            id: 8,
+            image: Img,
+            title: 'Prato',
+            fromValue: 15.00,
+            toValue: 10.00
+        }]
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col, Card } from 'react-bootstrap';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+import { Api } from './Api';
 
 function App() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark" fixed="top">
+    <Router>
+      <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Store</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
@@ -15,13 +25,54 @@ function App() {
         </Form>
       </Navbar>
 
-      <Container style={{paddingTop: '100px'}}>
-        <Row>
-          <Col>XIMIRAAAAAAAAAAAAAAAAAAAAAA</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
+      <Container style={{ paddingTop: '100px' }}>
+        <Switch>
+          <Route path="/product/:id" children={<Product />} />
+          <Route path="/">
+            <Row>
+              {Api.getItens().map(item => (
+                <Col>
+                  <Card style={{ width: '18rem', marginBottom: '1em' }}>
+                    <Link to={"/product/" + item.id}>
+                      <Card.Img variant="top" src={item.image} />
+                    </Link>
+                    <Card.Body>
+                      <Card.Title>{item.title}</Card.Title>
+                      <Card.Text>
+                        From: {item.fromValue}<br />
+                        To: {item.toValue}
+                      </Card.Text>
+                      <Button variant="primary">Add To Cart</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Route>
+        </Switch>
       </Container>
+    </Router>
+  );
+}
+
+function Product() {
+  let { id } = useParams();
+  const item = Api.getItens()[0];
+
+  return (
+    <>
+      <Link to="/">{id} Back</Link><br />
+
+      <Row>
+        <Col xs={4}><img src={item.image} alt="product" /></Col>
+        <Col xs={8}>
+          {item.title}<br />
+
+          From: {item.fromValue}<br />
+          To: {item.toValue}
+        </Col>
+      </Row>
+
     </>
   );
 }
